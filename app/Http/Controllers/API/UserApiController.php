d07bb01531316f82fea367d5ca964e36c5c1c1af
Chaow Porkaew <pchaow@gmail.com>
2015-05-05 11:27:00 +0700

    add user and role api controller and missing library

---
+++ app/Http/Controllers/API/UserApiController.php
@@ -0,0 +1,94 @@
+<?php namespace App\Http\Controllers\API;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use App\Services\UserService;
+use Illuminate\Http\Request;
+use \Input;
+
+class UserApiController extends Controller {
+
+    function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+
+    /**
+	 * Display a listing of the resource.
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+		return $this->userService->getAll();
+	}
+
+	/**
+	 * Show the form for creating a new resource.
+	 *
+	 * @return Response
+	 */
+	public function create()
+	{
+		return $this->userService->create();
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		return $this->userService->store(Input::all());
+	}
+
+	/**
+	 * Display the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		return $this->userService->get($id);
+	}
+
+	/**
+	 * Show the form for editing the specified resource.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function edit($id)
+	{
+        return $this->userService->get($id);
+
+    }
+
+	/**
+	 * Update the specified resource in storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function update($id)
+	{
+        return $this->userService->save(Input::all());
+
+    }
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		return $this->userService->delete($id);
+	}
+
+}
